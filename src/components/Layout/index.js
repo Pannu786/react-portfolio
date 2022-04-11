0ef7e531c76fc2c41c55174039e17ac5d87e2509
Pannu786 <thepannu786@gmail.com>
2022-04-11 17:33:33 +0530

    DEPARTING this project due to not enough knowledge of logo creation

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,14 @@
+import Sidebar from '../Sidebar';
 import './index.scss';
 
+//* this is a parent cmp
+
 const Layout = () => {
-  return <>Layout</>;
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
 };
 
 export default Layout;
